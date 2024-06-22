8e28c36c413220acc9def0060127e758b44ffd58
Non T <117605943+Ratanon691@users.noreply.github.com>
2024-06-23 00:54:13 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-// write code here
+let num = +prompt(`Give me a number`)
+let value
+switch (num) {
+  case 1:
+    value = `One`
+    break
+  case 0:
+    value = `Zero`
+    break
+  case -1:
+    value = `Minus`
+  break
+  default:
+    value = `Invalid number`
+}
+alert(`${value}`)
